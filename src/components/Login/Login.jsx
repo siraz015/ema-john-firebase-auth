7671cadda4ddfc48337903f928128c8f869b24b6
siraz015 <siraj015213@gmail.com>
2023-04-23 11:30:09 +0600

    implement password show/hide

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../Providers/AuthProvider';
 
 const Login = () => {
+    const [show, setShow] = useState(false);
 
     const { loginUser } = useContext(AuthContext);
     const navigate = useNavigate();
@@ -40,7 +41,12 @@ const Login = () => {
                 </div>
                 <div className="form-control">
                     <label htmlFor="">Password</label>
-                    <input className='password-form' type="password" name="password" id="" placeholder='Password' required />
+                    <input className='password-form' type={show ? 'text' : "password"} name="password" id="" placeholder='Password' required />
+                    <p onClick={() => setShow(!show)}><small>
+                        {
+                            show ? <span>Hide Password</span> : <span>Show Password</span>
+                        }
+                        </small></p>
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
                 <p><small>New to Ema-jhon? <Link to="/signup">Create New Account</Link></small></p>
